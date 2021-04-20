f7e4c213c1da2d738db6bfe6ebbe22b7d13ea811
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-04-20 15:50:46 +0200

    Loader fonts

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
@@ -48,6 +49,20 @@ Una vez importado el plugin, podemos desear el personalizarlos a través de opci
       {
         test: /\.png/,
         type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
       },
     ],
   },
